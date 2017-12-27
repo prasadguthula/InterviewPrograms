3402f1fcb934ebe541b1e00250613ee513afd16d
Prasad Guthula <prasad.guthula@valuelabs.com>
2017-12-27 17:05:45 +0530

    Commit Swapping numbers

--- src/Programs.java
+++ src/Programs.java
@@ -3,7 +3,18 @@ public class Programs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		swappingNumbers(10,20);
 
+	}
+	
+	public static void swappingNumbers(int a,int b) {
+		
+		int x=a;
+		a=b;
+		b=x;
+		System.out.println("First Value: "+a+"	Second Value: "+b);
+		
 	}
 
 }
